c69fa0f9a9270c8430c799d00c7cccd262954daa
gcraig14 <giscraig@gmail.com>
2020-03-30 13:14:20 -0300

    V2 with file browser

---
+++ descargas/index.php
@@ -0,0 +1,206 @@
+<?php
+require '../options.php';
+require '../views/header.php';
+
+?>
+
+<div class="wrap">
+    <?php
+
+    // FUNCTIONS TO MAKE THE MAGIC HAPPEN, BEST TO LEAVE THESE ALONE
+    function clean_title($title)
+    {
+        return ucwords( str_replace( array("-", "_"), " ", $title) );
+    }
+
+    function ext($filename)
+    {
+        return substr( strrchr( $filename,'.' ),1 );
+    }
+
+    function display_size($bytes, $precision = 2)
+    {
+        $units = array('B', 'KB', 'MB', 'GB', 'TB');
+        $bytes = max($bytes, 0);
+        $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+        $pow = min($pow, count($units) - 1);
+        $bytes /= (1 << (10 * $pow));
+        return round($bytes, $precision) . '<span class="fs-0-8 bold">' . $units[$pow] . "</span>";
+    }
+
+    function count_dir_files( $dir)
+    {
+        $fi = new FilesystemIterator(__DIR__ . "/" . $dir, FilesystemIterator::SKIP_DOTS);
+        return iterator_count($fi);
+    }
+
+    function get_directory_size($path)
+    {
+        $bytestotal = 0;
+        $path = realpath($path);
+        if($path!==false && $path!='' && file_exists($path))
+        {
+            foreach(new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path, FilesystemIterator::SKIP_DOTS)) as $object)
+            {
+                $bytestotal += $object->getSize();
+            }
+        }
+
+        return display_size($bytestotal);
+    }
+
+
+    // SHOW THE MEDIA BLOCK
+    function display_block( $file )
+    {
+        global $ignore_file_list, $ignore_ext_list, $force_download;
+
+        $file_ext = ext($file);
+        if( !$file_ext AND is_dir($file)) $file_ext = "dir";
+        if(in_array($file, $ignore_file_list)) return;
+        if(in_array($file_ext, $ignore_ext_list)) return;
+
+        $download_att = ($force_download AND $file_ext != "dir" ) ? " download='" . basename($file) . "'" : "";
+
+        $rtn = "<div class=\"block\">";
+        $rtn .= "<a href=\"$file\" class=\"$file_ext\"{$download_att}>";
+        $rtn .= "	<div class=\"img $file_ext\"></div>";
+        $rtn .= "	<div class=\"name\">";
+
+        if ($file_ext === "dir")
+        {
+            $rtn .= "		<div class=\"file fs-1-2 bold\">" . basename($file) . "</div>";
+            $rtn .= "		<div class=\"data upper size fs-0-7\"><span class=\"bold\">" . count_dir_files($file) . "</span> archivos</div>";
+            $rtn .= "		<div class=\"data upper size fs-0-7\"><span class=\"bold\">Tamaño:</span> " . get_directory_size($file) . "</div>";
+
+        }
+        else
+        {
+            $rtn .= "		<div class=\"file fs-1-2 bold\">" . basename($file) . "</div>";
+            $rtn .= "		<div class=\"data upper size fs-0-7\"><span class=\"bold\">Tamaño:</span> " . display_size(filesize($file)) . "</div>";
+        }
+
+        $rtn .= "	</div>";
+        $rtn .= "	</a>";
+        $rtn .= "</div>";
+        return $rtn;
+    }
+
+
+    // RECURSIVE FUNCTION TO BUILD THE BLOCKS
+    function build_blocks( $items, $folder )
+    {
+        global $ignore_file_list, $ignore_ext_list, $sort_by, $toggle_sub_folders, $ignore_empty_folders;
+
+        $objects = array();
+        $objects['directories'] = array();
+        $objects['files'] = array();
+
+        foreach($items as $c => $item)
+        {
+            if( $item == ".." OR $item == ".") continue;
+
+            // IGNORE FILE
+            if(in_array($item, $ignore_file_list)) { continue; }
+
+            if( $folder && $item )
+            {
+                $item = "$folder/$item";
+            }
+
+            $file_ext = ext($item);
+
+            // IGNORE EXT
+            if(in_array($file_ext, $ignore_ext_list)) { continue; }
+
+            // DIRECTORIES
+            if( is_dir($item) )
+            {
+                $objects['directories'][] = $item;
+                continue;
+            }
+
+            // FILE DATE
+            $file_time = date("U", filemtime($item));
+
+            // FILES
+            if( $item )
+            {
+                $objects['files'][$file_time . "-" . $item] = $item;
+            }
+        }
+
+        foreach($objects['directories'] as $c => $file)
+        {
+            $sub_items = (array) scandir( $file );
+
+            if( $ignore_empty_folders )
+            {
+                $has_sub_items = false;
+                foreach( $sub_items as $sub_item )
+                {
+                    $sub_fileExt = ext( $sub_item );
+                    if( $sub_item == ".." OR $sub_item == ".") continue;
+                    if(in_array($sub_item, $ignore_file_list)) continue;
+                    if(in_array($sub_fileExt, $ignore_ext_list)) continue;
+
+                    $has_sub_items = true;
+                    break;
+                }
+
+                if( $has_sub_items ) echo display_block( $file );
+            }
+            else
+            {
+                echo display_block( $file );
+            }
+
+            if( $toggle_sub_folders )
+            {
+                if( $sub_items )
+                {
+                    echo "<div class='sub' data-folder=\"$file\">";
+                    build_blocks( $sub_items, $file );
+                    echo "</div>";
+                }
+            }
+        }
+
+        // SORT BEFORE LOOP
+        if( $sort_by == "date_asc" ) { ksort($objects['files']); }
+        elseif( $sort_by == "date_desc" ) { krsort($objects['files']); }
+        elseif( $sort_by == "name_asc" ) { natsort($objects['files']); }
+        elseif( $sort_by == "name_desc" ) { arsort($objects['files']); }
+
+        foreach($objects['files'] as $t => $file)
+        {
+            $fileExt = ext($file);
+            if(in_array($file, $ignore_file_list)) { continue; }
+            if(in_array($fileExt, $ignore_ext_list)) { continue; }
+            echo display_block( $file );
+        }
+    }
+
+    // GET THE BLOCKS STARTED, FALSE TO INDICATE MAIN FOLDER
+    $items = scandir( dirname(__FILE__) );
+    build_blocks( $items, false );
+    ?>
+
+    <?php if($toggle_sub_folders) { ?>
+        <script type="text/javascript">
+            $(document).ready(function()
+            {
+                $("a.dir").click(function(e)
+                {
+                    $(this).toggleClass('open');
+                    $('.sub[data-folder="' + $(this).attr('href') + '"]').slideToggle();
+                    e.preventDefault();
+                });
+            });
+        </script>
+    <?php } ?>
+</div>
+
+<?php
+require '../views/footer.php';
+?>
---
+++ views/header.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contenidos Educativos Corrientes - Ministerio de Educacion de Corrientes</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+
+    <link rel="stylesheet" href="../style.css">
+    <script src="../scripts.js"></script>
+    <style>
+        .block .img { width: 50px; height: 50px; display: block; float: left; margin-right: 10px; background: transparent url(<?php echo $icon_url; ?>) no-repeat 0 0; }
+    </style>
+</head>
+
+<body>
+
+<div class="header p-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <img src="../assets/ctes-logo.png" alt="" class="ctes-logo">
+                <h1>Contenidos Educativos</h1>
+                <p>Dados los recientes acontecimientos de alcance mundial relacionados al virus COVID-19, El Ministerio de Educación de la Provincia de Corrientes pone a disposición de los tutores y alumnos, un conjunto de materiales educativos disponibles para descarga. De esta manera se busca garantizar la continuidad del crecimiento educativo en la provincia sin poner en riesgo la salud de los ciudadanos.</p>
+            </div>
+            <div class="col-sm-6 text-right">
+                <img src="../assets/online-ed.svg" alt="" class="main-img">
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="content">
+
