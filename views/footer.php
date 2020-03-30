b28b4907402b65c4a31623aca916671745d7987b
gcraig14 <giscraig@gmail.com>
2020-03-30 16:10:56 -0300

    Fix responsiveness

--- views/footer.php
+++ views/footer.php
@@ -1,29 +1,31 @@
     </div>
 </div>
-<div class="footer container-fluid p-5">
-    <div class="row">
-        <div class="col-md-4">
-            <div class="row">
-                <div class="col-5 pr-0" style="border-right:1px solid">
-                    <p>En colaboración con</p>
-                    <img src="../assets/ge-logo.png" alt="" style="width: 80%">
-                </div>
-                <div class="col-7 pl-5">
-                    <p><b>Contacto</b></p>
-                    <p>
-                        material-primaria@mec.gob.ar <br>
-                        material-secundaria@mec.gob.ar <br>
-                        material-orientado@mec.gob.ar <br>
-                    </p>
-                    <p>
-                        0379 - 4469755 <br>
-                        0379 - 4464558 <br>
-                        0800-888-2790 <br>
-                    </p>
+<div class="footer container-fluid pt-5 pb-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="row">
+                    <div class="col-md-5 pr-0 ge-logo">
+                        <p>En colaboración con</p>
+                        <img src="../assets/ge-logo.png" alt="" style="width: 80%">
+                    </div>
+                    <div class="col-md-7 pl-5">
+                        <p><b>Contacto</b></p>
+                        <p>
+                            material-primaria@mec.gob.ar <br>
+                            material-secundaria@mec.gob.ar <br>
+                            material-orientado@mec.gob.ar <br>
+                        </p>
+                        <p>
+                            0379 - 4469755 <br>
+                            0379 - 4464558 <br>
+                            0800-888-2790 <br>
+                        </p>
+                    </div>
                 </div>
             </div>
+            <div class="col-md-6"></div>
         </div>
-        <div class="col-md-8"></div>
     </div>
 </div>
 </body>
